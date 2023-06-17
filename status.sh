f611ff5770df53bf5aaeabdafc44bcd7b921e279
zahravpnn <136911092+zahravpnn@users.noreply.github.com>
2023-06-17 21:50:07 +0700

    Update status.sh

--- status.sh
+++ status.sh
@@ -132,8 +132,8 @@ clear;clear;clear
 
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo -e "                ${YELLOW}(${NC}${GREEN} STATUS SERVICE INFORMATION ${NC}${YELLOW})${NC}"
-echo -e "              OWNER : ${GREEN} BTR ${NC}${YELLOW}(${NC} ${GREEN}SC BY SETANTAZVPN ${NC}${YELLOW})${NC}"
-echo -e "       © Copyright By SETANTAZVPN TUNNEL ${YELLOW}(${NC} 2022-2023 ${YELLOW})${NC}"
+echo -e "              OWNER : ${GREEN} KLMPK ${NC}${YELLOW}(${NC} ${GREEN}SC BY Andyyuda ${NC}${YELLOW})${NC}"
+echo -e "       © Copyright By Andyyuda TUNNEL ${YELLOW}(${NC} 2022-2023 ${YELLOW})${NC}"
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo ""
 echo -e "${RED_BG}                     Sytem Information                    ${NC}"
